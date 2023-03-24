8f7b33ac84a1b97ddd44706eef732c68ed9f4f2b
Oleg <kovalyovop@yandex.ru>
2023-03-24 13:52:59 +0300

    Lesson 2.3

--- src/main/java/org/example/lesson2/Main.java
+++ src/main/java/org/example/lesson2/Main.java
@@ -1,21 +1,22 @@
 package org.example.lesson2;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        int a = 1;
-        int b = a++;
-        b += a++;
-
-       //System.out.println(b);
-       // System.out.println(a);
-
         String name = "Tom";
         int age = 30;
         float height = 1.7f;
-
         System.out.printf("Name: %s  Age: %d  Height: %.2f \n", name, age, height);
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input a number: ");
+        int num = in.nextInt();
+
+        System.out.printf("Your number: %d \n", num);
+        in.close();
 
     }
 }
