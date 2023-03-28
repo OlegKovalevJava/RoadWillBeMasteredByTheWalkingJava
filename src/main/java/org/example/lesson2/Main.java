9dad42a897608ec79dee7fd3d7235dc60bded949
Oleg <kovalyovop@yandex.ru>
2023-03-29 00:01:57 +0300

    Lesson 17

--- src/main/java/org/example/lesson2/Main.java
+++ src/main/java/org/example/lesson2/Main.java
@@ -6,11 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String name = "Tom";
-        int age = 30;
-        float height = 1.7f;
-        System.out.printf("Name: %s  Age: %d  Height: %.2f \n", name, age, height);
-
         Scanner in = new Scanner(System.in);
         System.out.print("Input a number: ");
         int num = in.nextInt();
