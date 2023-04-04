cb88b65bd815b9a0f94ec28b5b30d097ed41380b
Oleg <kovalyovop@yandex.ru>
2023-04-04 20:51:20 +0300

    Lesson 19

---
+++ src/main/java/org/example/lesson5/Main.java
@@ -0,0 +1,7 @@
+package org.example.lesson5;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/org/example/lesson7/Main.java
@@ -0,0 +1,7 @@
+package org.example.lesson7;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
