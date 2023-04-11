5f1fa7639bdaf6a204888a9ff066d9999bb2292f
Oleg <kovalyovop@yandex.ru>
2023-04-11 22:03:36 +0300

    Lesson 22.1

--- src/main/java/org/example/lesson22/Main.java
+++ src/main/java/org/example/lesson22/Main.java
@@ -1,7 +1,16 @@
 package org.example.lesson22;
 
+// ОБЕРТКИ ДЛЯ ПРИМИТИВНЫХ ТИПОВ ДАННЫХ
+
 public class Main {
     public static void main(String[] args) {
-
+        Integer i = 5;
+        Integer j = 5;
+        if (i.equals(j)) {
+            System.out.println("СОВПАДАЮТ");
+        }
+        if (i == j) {
+            System.out.println("СОВПАДАЮТ");
+        }
     }
 }
