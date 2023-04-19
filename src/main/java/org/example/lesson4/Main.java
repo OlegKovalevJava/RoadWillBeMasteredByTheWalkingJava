e56ed346fe32c0853e7eead79b455cb34803589d
Oleg <kovalyovop@yandex.ru>
2023-04-19 13:53:55 +0300

    Lesson 4.1

--- src/main/java/org/example/lesson4/Main.java
+++ src/main/java/org/example/lesson4/Main.java
@@ -3,5 +3,10 @@ package org.example.lesson4;
 public class Main {
     public static void main(String[] args) {
 
+            boolean isTrue = false;
+            
+            if (!isTrue) {
+                System.out.println("TRUE");
+            }
     }
 }
