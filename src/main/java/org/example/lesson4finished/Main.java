ca60036879ec702287f1033b18bef39ce3cf029f
Oleg <kovalyovop@yandex.ru>
2023-04-20 01:00:30 +0300

    Lesson 4finished

---
+++ src/main/java/org/example/lesson4finished/Main.java
@@ -0,0 +1,20 @@
+package org.example.lesson4finished;
+
+public class Main {
+    public static void main(String[] args) {
+
+        final int numOne = 10;
+        final int numTwo = 100;
+
+        if (numOne < numTwo) {
+            System.out.println("One");
+        } else if (numOne < numTwo) {
+            System.out.println("Two");
+        } else if (numOne < numTwo) {
+            System.out.println("Three");
+        } else {
+            System.out.println("No");
+        }
+
+    }
+}
