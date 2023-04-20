5bf099e9b7f614b96983bc759f4d00b346e16ff9
Oleg <kovalyovop@yandex.ru>
2023-04-20 21:32:53 +0300

    Lesson 5.2

--- src/main/java/org/example/lesson5/Lesson5.java
+++ src/main/java/org/example/lesson5/Lesson5.java
@@ -3,9 +3,27 @@ package org.example.lesson5;
 public class Lesson5 {
     public static void main(String[] args) {
 
-        int[] numbers = {1, 2, 3, 4, 5};
-        numbers[0] = 50;
-        System.out.println(numbers[0]);
+        int[] number = {1, 2, 3, 4, 5};
+        System.out.println(number.length);
+
+        int last = number[number.length - 1];
+        System.out.println(last);
+
+        for (int i : number) {
+            System.out.println(i);
+        }
+
+        int n = 10;
+
+        for (int i = 0, j = n - 1; i < j; i++, j--) {
+            System.out.println(i * j);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            if (i == 5)
+                continue;
+            System.out.println(i);
+        }
 
     }
 }
