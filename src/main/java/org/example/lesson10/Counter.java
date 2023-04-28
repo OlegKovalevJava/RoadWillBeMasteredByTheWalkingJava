3866a62f3346eb4b360d53f0279a49f0244c7be7
Oleg <kovalyovop@yandex.ru>
2023-04-28 23:17:58 +0300

    Lesson 10.1

---
+++ src/main/java/org/example/lesson10/Counter.java
@@ -0,0 +1,7 @@
+package org.example.lesson10;
+
+public class Counter {
+
+    public static int counter;
+
+}
--- src/main/java/org/example/lesson10/Main.java
+++ src/main/java/org/example/lesson10/Main.java
@@ -2,6 +2,10 @@ package org.example.lesson10;
 
 public class Main {
     public static void main(String[] args) {
+        Counter.counter++;
+        Counter.counter++;
+
+        System.out.println(Counter.counter + " " + Counter.counter);
 
     }
 }
