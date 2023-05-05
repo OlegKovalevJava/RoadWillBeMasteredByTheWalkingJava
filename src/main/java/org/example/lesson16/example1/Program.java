32ee91f21347b03445b375b9f021d63360b785bf
Oleg <kovalyovop@yandex.ru>
2023-05-05 20:46:37 +0300

    Lesson 16.1

---
+++ src/main/java/org/example/lesson16/example1/Program.java
@@ -0,0 +1,13 @@
+package org.example.lesson16.example1;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Employee sam = new Employee("Sam", "Sber");
+        sam.display();
+
+        Client bob = new Client("Bob", "Alfa");
+        bob.display();
+
+    }
+}
