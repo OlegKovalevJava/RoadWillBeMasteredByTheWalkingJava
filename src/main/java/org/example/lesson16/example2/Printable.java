65e92eca84a9237cf5023cdb63edbe6106134392
Oleg <kovalyovop@yandex.ru>
2023-05-06 01:47:53 +0300

    Lesson 16.3

--- src/main/java/org/example/lesson16/example2/Printable.java
+++ src/main/java/org/example/lesson16/example2/Printable.java
@@ -1,18 +1,10 @@
 package org.example.lesson16.example2;
 
 public interface Printable {
-
     void print();
-
-    int OPEN = 1;
-    int CLOSED = 0;
 
     default int sum(int a, int b) {
         return sumAll(a, b);
-    }
-
-    default int sum(int a, int b, int c) {
-        return sumAll(a, b, c);
     }
 
     private int sumAll(int... values) {
