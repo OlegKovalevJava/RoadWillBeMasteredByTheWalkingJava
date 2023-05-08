67f690fda9c55e9c4ad702d39060356fd28d3cb4
Oleg <kovalyovop@yandex.ru>
2023-05-08 16:58:39 +0300

    Lesson 17.2

--- src/main/java/org/example/lesson17/Program.java
+++ src/main/java/org/example/lesson17/Program.java
@@ -1,8 +1,14 @@
 package org.example.lesson17;
 
-public class Program{
+public class Program {
     public static void main(String[] args) {
+        System.out.println(Color.RED.getCode());
+        System.out.println(Color.BLUE.getCode());
+        System.out.println(Color.GREEN.getCode());
 
-
+        Color[] colors = Color.values();
+        for (Color c : colors) {
+            System.out.println(c);
+        }
     }
 }
