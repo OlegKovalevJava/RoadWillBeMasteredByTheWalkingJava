f42e6a0e0ba82c603f8c96a81c6e3e9f33a2b427
Oleg <kovalyovop@yandex.ru>
2023-05-23 22:21:16 +0300

    Lesson 26

---
+++ src/main/java/lesson26/Main.java
@@ -0,0 +1,11 @@
+package lesson26;
+
+public class Main {
+    public static void main(String[] args) {
+        String word = "avadakedavra";
+        System.out.println(word.charAt(1));
+        System.out.println(word.isEmpty());
+
+    }
+}
+
