368d0dd941a9f0386bf25ae40eaae1b08279a548
Oleg <kovalyovop@yandex.ru>
2023-05-24 21:29:31 +0300

    Lesson 26.1

--- src/main/java/lesson26/Main.java
+++ src/main/java/lesson26/Main.java
@@ -2,10 +2,15 @@ package lesson26;
 
 public class Main {
     public static void main(String[] args) {
-        String word = "avadakedavra";
-        System.out.println(word.charAt(1));
-        System.out.println(word.isEmpty());
+        StringBuilder word = new StringBuilder("word");
+        StringBuilder wordTwo = new StringBuilder("wordTwo");
+        String wordThree = "wordThree";
+
+        System.out.println("compareTo: " + word.compareTo(wordTwo));
+        //System.out.println("delete: " + word.delete(0, 2));
+        System.out.println("deleteCharAt: " + word.deleteCharAt(3));
+        System.out.println("replace " + word.replace(1,2,wordThree));
+        System.out.println("reverse " + word.reverse());
 
     }
 }
-
