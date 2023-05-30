3a934c475db016f5c77335eab2b17db802a5db1a
Oleg <kovalyovop@yandex.ru>
2023-05-30 23:37:16 +0300

    Lesson 27.3

---
+++ src/main/java/org/example/finished/lesson27/Debugger.java
@@ -0,0 +1,27 @@
+package org.example.finished.lesson27;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Debugger {
+    List<String> words = new ArrayList<>();
+
+    int[] myArray = new int[2];
+
+    void fillingInLines() {
+        words.add("name");
+        for (String words : words) {
+            System.out.println(words);
+        }
+    }
+
+    void fillingInNumbers() {
+        myArray[0] = 111;
+        myArray[1] = 222;
+
+        for (int numbers : myArray) {
+            System.out.println("если myArray " + myArray);
+            System.out.println("если numbers " + numbers);
+        }
+    }
+}
---
+++ src/main/java/org/example/finished/lesson27/Main.java
@@ -0,0 +1,8 @@
+package org.example.finished.lesson27;
+
+public class Main {
+    public static void main(String[] args) {
+      Debugger debugger = new Debugger();
+      debugger.fillingInNumbers();
+    }
+}
