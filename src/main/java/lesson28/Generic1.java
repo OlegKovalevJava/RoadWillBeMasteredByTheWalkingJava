13953e4965c23ff0ce1080d84e82e755852779e7
Oleg <kovalyovop@yandex.ru>
2023-05-31 22:12:35 +0300

    Lesson 28.2

--- src/main/java/lesson28/Generic1.java
+++ src/main/java/lesson28/Generic1.java
@@ -1,16 +1,12 @@
 package lesson28;
 
-//Параметризуем класс generic T. T может быть любым ссылочным типом
-public class Generic1<T> {
-    //Объявляем поле типа T. В конечном итоге для каждого экземпляра T будет соответствовать конкретному типу данных
+public class Generic1<T extends Number> {
     private T field;
 
-    //Метод, возвращающий объект, соответствующий типу T для данного экземпляра Generic1
     public T getField() {
         return field;
     }
 
-    //Метод, принимающий параметр, соответствующий типу T для данного экземпляра Generic1
     public void setField(T field) {
         this.field = field;
     }
