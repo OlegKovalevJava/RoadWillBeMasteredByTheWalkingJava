7e088975fa562706e3e7a1fab4aebb538c6a3575
Oleg <kovalyovop@yandex.ru>
2023-06-13 20:00:20 +0300

    Lesson 31.2

--- src/main/java/lesson31/Main.java
+++ src/main/java/lesson31/Main.java
@@ -5,11 +5,6 @@ import java.io.InputStream;
 
 public class Main {
     public static void main(String[] args) {
-        InputStream inputStream = new InputStream() {
-            @Override
-            public int read() throws IOException {
-                return 0;
-            }
-        }
-            }
+
+    }
 }
