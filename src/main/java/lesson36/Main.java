5e255d69efd9b428dcae15d77598a3a06a445282
Oleg <kovalyovop@yandex.ru>
2023-06-29 22:22:59 +0300

    Lesson 36.4

--- src/main/java/lesson36/Main.java
+++ src/main/java/lesson36/Main.java
@@ -1,14 +1,7 @@
 package lesson36;
 
-public class Main {
+public class Main implements Iterable{
     public static void main(String[] args) {
-
-        char c = 'a';
-        char b = 'a' + 1;
-
-        System.out.println(c);
-        System.out.println(b);
-
 
     }
 }
