453d25ea34b41cfb75dfbf7d8f568a3a611cd52d
Oleg <kovalyovop@yandex.ru>
2023-07-03 21:14:32 +0300

    Lesson 36.10

--- src/main/java/lesson36/Main.java
+++ src/main/java/lesson36/Main.java
@@ -1,7 +1,13 @@
 package lesson36;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+
+        List<String> stringList = new ArrayList<>();
+
 
     }
 }
