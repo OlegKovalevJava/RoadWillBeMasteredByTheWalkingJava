4c264053b152ca0c8f13b1f5e3de23e898b9c6f2
Oleg <kovalyovop@yandex.ru>
2023-07-14 09:00:28 +0300

    Lesson 38.2

--- src/main/java/lesson38/Main.java
+++
@@ -1,5 +0,0 @@
-package lesson38;
-
-public class Main {
-
-}
---
+++ src/main/java/lesson38/Test1.java
@@ -0,0 +1,13 @@
+package lesson38;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test1 {
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+
+    }
+
+}
