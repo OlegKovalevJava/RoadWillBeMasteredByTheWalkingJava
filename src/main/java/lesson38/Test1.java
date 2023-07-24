c3b32fe983d91cfc6c55a9bd892dbc69de98b981
Oleg <kovalyovop@yandex.ru>
2023-07-24 20:40:18 +0300

    Lesson 38.3

--- src/main/java/lesson38/Test1.java
+++ src/main/java/lesson38/Test1.java
@@ -1,13 +1,17 @@
 package lesson38;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.TreeSet;
 
 public class Test1 {
     public static void main(String[] args) {
 
-        List<String> list = new ArrayList<>();
+        TreeSet<Person> people = new TreeSet<Person>();
+        people.add(new Person("Tom"));
+        people.add(new Person("Tom"));
+        people.add(new Person("Mot"));
+
+
+
 
     }
-
 }
