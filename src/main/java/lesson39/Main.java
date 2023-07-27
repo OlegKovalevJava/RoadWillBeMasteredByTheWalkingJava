a00f026fd45dfb89760c7522cc0304728b955a6a
Oleg <kovalyovop@yandex.ru>
2023-07-27 22:45:36 +0300

    Lesson 39.1

--- src/main/java/lesson39/Main.java
+++ src/main/java/lesson39/Main.java
@@ -1,7 +1,19 @@
 package lesson39;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Main {
     public static void main(String[] args) {
+        Queue <String> queue = new LinkedList<>();
+        queue.add("Oleg");
+        queue.add("Ivan");
+        queue.add("Max");
+        System.out.println(queue);
+        System.out.println(queue.remove());
+        System.out.println(queue.remove());
+        System.out.println(queue.remove());
+        System.out.println(queue);
 
     }
 }
