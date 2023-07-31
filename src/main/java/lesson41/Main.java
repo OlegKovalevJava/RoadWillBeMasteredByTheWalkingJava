9bfd808d16f8538660d37112882f40fd7f0fc0a8
Oleg <kovalyovop@yandex.ru>
2023-07-31 21:38:22 +0300

    Lesson 41.1

--- src/main/java/lesson41/Main.java
+++ src/main/java/lesson41/Main.java
@@ -1,7 +1,16 @@
 package lesson41;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
+        Set<String> set = new HashSet<>();
+        set.add("Maz");
+        set.add("Maz");
+        set.add("maz");
+        set.add("maz");
+        System.out.println(set);
 
     }
 }
