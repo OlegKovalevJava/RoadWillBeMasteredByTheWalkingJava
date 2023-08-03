2ce1c28378741f4478ce99cb0ad8cb4f6521b4f8
Oleg <kovalyovop@yandex.ru>
2023-08-03 19:17:42 +0300

    skillFactory String1

--- src/main/java/skillfactory/array/StringEx.java
+++ src/main/java/skillfactory/array/StringEx.java
@@ -1,16 +1,22 @@
 package skillfactory.array;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class StringEx {
     public static void main(String[] args) {
 
-        String name = "Ivan";
-        Integer age = 77;
-
-        String phrase = "My name is " + name + " I'm is " + age +" age old";
-        System.out.println(phrase);
-
-        String formatPhrase = String.format("My name is %s I'm is %d age old", name, age);
-        System.out.println(formatPhrase);
+        String strDate = "Sat, April 4, 2020";
+        SimpleDateFormat formatter = new SimpleDateFormat(", MMMM d, yyyy", Locale.ENGLISH);
+        try {
+            Date date = formatter.parse(strDate);
+            System.out.println(formatter.format(date));
+        }
+        catch (ParseException e) {
+            e.printStackTrace();
+        }
 
     }
 }
