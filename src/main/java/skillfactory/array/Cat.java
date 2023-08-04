bb2fdcff4b7311555c25d958ff8a861a997a1201
Oleg <kovalyovop@yandex.ru>
2023-08-04 19:46:19 +0300

    skillFactory Cat1

--- src/main/java/skillfactory/array/Cat.java
+++ src/main/java/skillfactory/array/Cat.java
@@ -1,23 +1,27 @@
 package skillfactory.array;
 
 public class Cat {
-    private double weight;
-    private String name = "NO";
 
+    private double weight;
+    private String name;
+    private int age;
+    private String color;
+
+    //Конструктор по умолчанию
     public Cat() {
+
     }
 
-    public Cat(double weight, String name) {
+    //Конструктор, принимающий параметры
+    public Cat(double weight, String name, int age, String color) {
         this.weight = weight;
         this.name = name;
+        this.age = age;
+        this.color = color;
     }
 
     public double getWeight() {
         return weight;
-    }
-
-    public void setWeight(double weight) {
-        this.weight = weight;
     }
 
     public String getName() {
@@ -26,5 +30,39 @@ public class Cat {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        //кошки не молодеют!
+        if (age > this.age)
+            this.age = age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+    //метод только для садистов
+    public void setColor(String color) {
+        this.color = color;
+        //и так жалобно
+        meow(); meow(); meow();
+    }
+    public void meow() {
+        System.out.println("Meow");
+    }
+
+    private void pee() {
+        weight -= 20;
+    }
+
+    public void eat() {
+        weight += 50;
+        //сами вызываем свою функцию, если надо
+        if (50 / weight > .01)
+            pee();
     }
 }
--- src/main/java/skillfactory/array/Main.java
+++ src/main/java/skillfactory/array/Main.java
@@ -3,10 +3,8 @@ package skillfactory.array;
 public class Main {
     public static void main(String[] args) {
 
-        Cat cat1 = new Cat();
-        cat1.setWeight(11.1);
 
-        System.out.println(cat1.getName() + " " + cat1.getWeight());
+
 
     }
 }
