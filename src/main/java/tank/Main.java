abdb8abaeddb3beefc4309326e3b76e29ebeca14
Oleg <kovalyovop@yandex.ru>
2023-08-08 22:34:57 +0300

    skillFactory Tank1

--- src/main/java/tank/Main.java
+++ src/main/java/tank/Main.java
@@ -2,6 +2,12 @@ package tank;
 
 public class Main {
     public static void main(String[] args) {
-
+        Tank tank = new Tank(0);
+        tank.goForward(10);
+        tank.printPosition();
+        tank.goForward(50);
+        tank.printPosition();
+        tank.goBackward(100);
+        tank.printPosition();
     }
 }
--- src/main/java/tank/Tank.java
+++ src/main/java/tank/Tank.java
@@ -1,9 +1,22 @@
 package tank;
 
 public class Tank {
-    int position;
+    private int position;
 
-    void goForward(int distance) {
+    public Tank(int position) {
+        this.position = position;
+    }
+
+    public void goForward(int distance) {
         this.position = position + distance;
+    }
+
+    public void goBackward(int distance) {
+        this.position = position - distance;
+    }
+
+    public void printPosition() {
+        System.out.printf("The Tank is at %d now.", position);
+        System.out.println();
     }
 }
