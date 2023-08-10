3d6730fa80fa14732905c8e76b75787bcaa66157
Oleg <kovalyovop@yandex.ru>
2023-08-10 12:28:44 +0300

    skillFactory Tank4

--- src/main/java/tank/Main.java
+++ src/main/java/tank/Main.java
@@ -3,5 +3,9 @@ package tank;
 public class Main {
     public static void main(String[] args) {
 
+        Tank tank = new Tank();
+        tank.goForward(-1000);
+        tank.printPosition();
+
     }
 }
