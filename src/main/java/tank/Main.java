505883c071cafbb62ac5637fddc3b690d407f735
Oleg <kovalyovop@yandex.ru>
2023-08-10 12:27:06 +0300

    skillFactory Tank4

--- src/main/java/tank/Main.java
+++ src/main/java/tank/Main.java
@@ -3,15 +3,5 @@ package tank;
 public class Main {
     public static void main(String[] args) {
 
-        Tank tank = new Tank();
-        tank.goForward(10);
-        tank.goForward(10);
-        tank.printPosition();
-        tank.turnRight();
-        tank.goForward(50);
-        tank.printPosition();
-        tank.turnLeft();
-        tank.goBackward(100);
-        tank.printPosition();
     }
 }
--- src/main/java/tank/Tank.java
+++ src/main/java/tank/Tank.java
@@ -1,15 +1,45 @@
 package tank;
 
+import lombok.*;
+
+@Getter
+@Setter
 public class Tank {
-    int x;
-    int y;
-    int dir;
+    private int x;
+    private int y;
+    private int dir;
+    private int fuel = 100;
+
+    public Tank() {
+        this(0, 0, 100);
+    }
+
+    public Tank(int x, int y) {
+        this(x, y, 100);
+    }
+
+    public Tank(int x, int y, int fuel) {
+        this.x = x;
+        this.y = y;
+        this.fuel = fuel;
+    }
 
     public void goForward(int i) {
-        if (dir == 0) x += i;
-        else if (dir == 1) y += i;
-        else if (dir == 2) x -= i;
-        else y -= i;
+        if (i < 0 && -i > -fuel) { // 101
+            i = -fuel;
+        } else if (i > fuel) {
+            i = fuel;
+        }
+
+        if (dir == 0) {
+            x += i;
+        } else if (dir == 1) {
+            y += i;
+        } else if (dir == 2) {
+            x -= i;
+        } else y -= i;
+
+        fuel = fuel - Math.abs(i);
     }
 
     public void goBackward(int i) {
@@ -18,12 +48,16 @@ public class Tank {
 
     public void turnLeft() {
         dir--;
-        if (dir == -1) dir = 3;
+        if (dir == -1) {
+            dir = 3;
+        }
     }
 
     public void turnRight() {
         dir++;
-        if (dir == 4) dir = 0;
+        if (dir == 4) {
+            dir = 0;
+        }
     }
 
     public void printPosition() {
