1628e37bdea07dbff626f7c01b77f95460b1e111
Oleg <kovalyovop@yandex.ru>
2023-08-16 13:47:17 +0300

    skillFactory4

--- src/main/java/skill/Main.java
+++
@@ -1,9 +0,0 @@
-package skill;
-
-public class Main {
-    public static void main(String[] args) {
-        Human ivan = new Human("Ivan", "Moscow");
-        System.out.println(ivan.toString());
-
-    }
-}
