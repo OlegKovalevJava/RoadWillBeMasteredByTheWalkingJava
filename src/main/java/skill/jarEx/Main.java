a26a942f0b8526c47444c74342d8a0f8547b588a
Oleg <kovalyovop@yandex.ru>
2023-08-24 23:32:19 +0300

    skillFactoryJAR1

--- src/main/java/skill/jarEx/Main.java
+++ src/main/java/skill/jarEx/Main.java
@@ -1,7 +1,14 @@
 package skill.jarEx;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        File file = new File("C:\\Test");
+        String[] files = file.list();
+        System.out.println(files.length);
+
 
     }
 }
