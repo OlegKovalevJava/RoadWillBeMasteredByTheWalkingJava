68eb6eb2e11f9ae76adc0cfba0d991c26871d8c9
Oleg <kovalyovop@yandex.ru>
2023-08-30 13:08:03 +0300

    skillFactoryRAndW3

---
+++ src/main/java/finished/lesson42/Main.java
@@ -0,0 +1,13 @@
+package finished.lesson42;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        FileInputStream fis;
+
+
+    }
+}
