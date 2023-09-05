392e075fd371aa3690a145c9cbe0090963466f8b
Oleg <kovalyovop@yandex.ru>
2023-09-05 22:39:13 +0300

    skill3

--- src/main/java/skillFactory/Main.java
+++ src/main/java/skillFactory/Main.java
@@ -4,9 +4,9 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int[] array = {1, 2, -1, 5, 3};
+        int[] array = {0, 1, 2, 3, 4};
         int s = 0;
-        for (int i = 0; i <= array.length; i++) {
+        for (int i = 0; i < array.length; i++) {
             s += array[i];
         }
         System.out.println(s);
