dcd3f214418caee63bf420a756719a65d3613aff
Oleg <kovalyovop@yandex.ru>
2023-09-07 19:27:42 +0300

    skill5

--- src/main/java/skillFactory/Main.java
+++ src/main/java/skillFactory/Main.java
@@ -9,10 +9,6 @@ public class Main {
         Car car1 = new Car(4, "max");
         Car car2 = new Car(6, "MIX");
         cars.add(car1);
-        cars.add(car1);
-        cars.add(car1);
-        cars.add(car2);
-        cars.add(car2);
         cars.add(car2);
         for (Car c: cars) {
             System.out.println(c.toString());
