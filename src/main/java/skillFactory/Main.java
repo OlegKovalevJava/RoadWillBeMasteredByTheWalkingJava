60e5b90ead1fd4501f03b14c68f99511918eea13
Oleg <kovalyovop@yandex.ru>
2023-09-09 00:26:46 +0300

    skillEx2

--- src/main/java/skillFactory/Main.java
+++ src/main/java/skillFactory/Main.java
@@ -4,36 +4,32 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        if (copyFileUsingStream("src/utf8.txt", "UTF-8",
-                "src/win1251.txt", "windows-1251")) {
-            System.out.println("Перекодировка прошла успешно!");
-        }
 
+        try {
+            copyFileUsingStream("src/utf.txt", "UTF-8", "src/win1251.txt", "windows-1251");
+            System.out.println("Перекодировка прошла успешно");
+        } catch (ConvertException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 
-    private static boolean copyFileUsingStream(String sourceFileName, String sourceEnc,
-                                               String destFileName, String descEnc) {
-
-        try (Reader fis = new InputStreamReader(new FileInputStream((sourceFileName)), sourceEnc);
-             Writer fos = new OutputStreamWriter(new FileOutputStream((destFileName)), descEnc)) {
+    private static void copyFileUsingStream(String sourceFilename, String sourceEnc,
+                                            String destFilename, String destEnc) throws ConvertException {
+        try (Reader fis = new InputStreamReader(new FileInputStream(new File(sourceFilename)), sourceEnc);
+             Writer fos = new OutputStreamWriter(new FileOutputStream(new File(destFilename)), destEnc)) {
 
             char[] buffer = new char[1024];
             int length;
             while ((length = fis.read(buffer)) > 0) {
                 fos.write(buffer, 0, length);
             }
-
         } catch (FileNotFoundException ex) {
-            System.out.println("Не удалось открыть файл " + ex.getMessage());
-            return false;
+            throw new ConvertException("Не удалось открыть файл " + ex.getMessage());
         } catch (UnsupportedEncodingException ex) {
-            System.out.println("Указана неизвестная кодировка " + ex.getMessage());
-            return false;
+            throw new ConvertException("Указана неизвестная кодировка " + ex.getMessage());
         } catch (IOException ex) {
-            System.out.println("Возникла ошибка при копировании");
-            return false;
+            throw new ConvertException("Возникла ошибка при копировании");
         }
-        return true;
     }
 
 }
