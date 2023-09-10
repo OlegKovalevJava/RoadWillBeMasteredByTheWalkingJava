bd3b168d911a4fe4e885c488f76c00c04133548a
Oleg <kovalyovop@yandex.ru>
2023-09-10 20:53:35 +0300

    skillEx4

--- src/main/java/skillFactory/collection/Main.java
+++ src/main/java/skillFactory/collection/Main.java
@@ -1,7 +1,18 @@
 package skillFactory.collection;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+
+        char[] chars = new char[3];
+
+        chars[0] = 'a';
+        chars[1] = 'A';
+        chars[2] = 's';
+
+        Arrays.sort(chars);
+        System.out.println(chars);
 
     }
 }
