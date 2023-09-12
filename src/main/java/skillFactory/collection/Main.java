bda72ce8bc6438527bfcc37cf037ae07a07495d7
Oleg <kovalyovop@yandex.ru>
2023-09-12 19:20:59 +0300

    skill - collection

--- src/main/java/skillFactory/collection/Main.java
+++ src/main/java/skillFactory/collection/Main.java
@@ -5,7 +5,23 @@ import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) {
-        Queue queue = new ArrayDeque();
-        System.out.println(((ArrayDeque<?>) queue).pop());
+
+        int numberOfTask = 5;
+
+        Queue toDoQueue = new ArrayDeque(numberOfTask);
+
+        Boss boss = new Boss(toDoQueue);
+
+        Worker worker = new Worker(toDoQueue);
+
+        for (int i = 1; i <= numberOfTask; i++) {
+            boss.giveTask("Задание " + i);
+        }
+
+        System.out.println("Начальник закончил давать задания");
+
+        for (int i = 1; i <= numberOfTask + 1; i++) {
+            worker.takeTask();
+        }
     }
 }
