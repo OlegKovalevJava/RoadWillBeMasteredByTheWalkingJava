740eb41d74b6e89ddc97bf6745b2c83ae97d3917
Oleg <kovalyovop@yandex.ru>
2023-09-13 13:57:29 +0300

    skill - collection1

--- src/main/java/skillFactory/collection2/Crate.java
+++ src/main/java/skillFactory/collection2/Crate.java
@@ -1,16 +1,10 @@
 package skillFactory.collection2;
 
 public class Crate<T> {
-    private T contents;
-    public Crate(T contents) {
-        this.contents = contents;
+
+    public static <T> Crate<T> ship(T stuff) {
+        System.out.println("Preparing " + stuff);
+        return new Crate<T>();
     }
 
-    public T getContents() {
-        return contents;
-    }
-
-    public void setContents(T contents) {
-        this.contents = contents;
-    }
 }
--- src/main/java/skillFactory/collection2/Main.java
+++ src/main/java/skillFactory/collection2/Main.java
@@ -1,11 +1,7 @@
 package skillFactory.collection2;
 
-import java.util.ArrayDeque;
-import java.util.Queue;
-
 public class Main {
     public static void main(String[] args) {
-
 
     }
 }
