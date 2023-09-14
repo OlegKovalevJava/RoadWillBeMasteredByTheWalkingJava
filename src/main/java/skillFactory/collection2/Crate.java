fb94e21f169d8c418bb592d41f1a284ed5e58ecd
Oleg <kovalyovop@yandex.ru>
2023-09-14 22:01:25 +0300

    skill - collection2

--- src/main/java/skillFactory/collection2/Crate.java
+++ src/main/java/skillFactory/collection2/Crate.java
@@ -2,6 +2,23 @@ package skillFactory.collection2;
 
 public class Crate<T> {
 
+    private T contents;
+
+    public Crate() {
+    }
+
+    public Crate(T contents) {
+        this.contents = contents;
+    }
+
+    public T getContents() {
+        return contents;
+    }
+
+    public void setContents(T contents) {
+        this.contents = contents;
+    }
+
     public static <T> Crate<T> ship(T stuff) {
         System.out.println("Preparing " + stuff);
         return new Crate<T>();
--- src/main/java/skillFactory/collection2/Main.java
+++ src/main/java/skillFactory/collection2/Main.java
@@ -1,7 +1,23 @@
 package skillFactory.collection2;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Main {
     public static void main(String[] args) {
+        Queue<String> keywords = new LinkedList<>();
+        keywords.add("Java");
+        keywords.add("Java");
+        keywords.add("Java");
+        keywords.add("Java");
 
+        printList(keywords);
     }
+
+    private static void printList(Queue<?> keywords) {
+        for (Object o : keywords) {
+            System.out.println(o);
+        }
+    }
+
 }
