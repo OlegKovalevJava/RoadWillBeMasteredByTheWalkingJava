837d0ddcbfea78561a847a342c5659d9de45ddfd
Oleg <kovalyovop@yandex.ru>
2023-10-17 22:00:01 +0300

    Optional

--- src/main/java/finished/skill/optional/Main.java
+++ src/main/java/finished/skill/optional/Main.java
@@ -3,14 +3,5 @@ package finished.skill.optional;
 import java.util.Optional;
 
 public class Main {
-    public static Optional<Double> average(int... scores) {
-        if (scores.length == 0) {
-            return Optional.empty(); // строка 1
-        }
-        int sum = 0;
-        for (int score : scores) {
-            sum += score;
-        }
-        return Optional.of((double) sum / scores.length); // строка 2
-    }
+
 }
