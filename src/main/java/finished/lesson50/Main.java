8a2654993c237cc9afb228f958a485310e6aefe3
Oleg <kovalyovop@yandex.ru>
2023-10-20 23:07:24 +0300

    Streams1

--- src/main/java/finished/lesson50/Main.java
+++ src/main/java/finished/lesson50/Main.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Person> personList = Data.getPersons();
+        List<Person> personList2 = personList.stream().distinct();
 
         for (int i = 0; i < personList.size(); i++) {
             System.out.println(personList.get(i));
