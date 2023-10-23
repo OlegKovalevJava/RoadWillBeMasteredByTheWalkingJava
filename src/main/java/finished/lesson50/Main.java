8046bd5e6c8e6893df8b2fa3b244b5c533631dbc
Oleg <kovalyovop@yandex.ru>
2023-10-23 13:34:53 +0300

    Streams1

--- src/main/java/finished/lesson50/Main.java
+++ src/main/java/finished/lesson50/Main.java
@@ -1,14 +1,13 @@
 package finished.lesson50;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         List<Person> persons = Data.getPersons();
 
         List<Person> persons2 = persons.stream()
-                .filter(person -> person.getAge() > 30)
+                .filter(person -> person.getAge() > 40)
                 .toList();
 
         for (int i = 0; i < persons2.size(); i++) {
