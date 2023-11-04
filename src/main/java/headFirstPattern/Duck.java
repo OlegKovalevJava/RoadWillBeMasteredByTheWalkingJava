184471049cebfb1884e98b3864b3f9fa4c1e8209
Oleg <kovalyovop@yandex.ru>
2023-11-04 03:20:04 +0300

    headFirstPattern

---
+++ src/main/java/headFirstPattern/Duck.java
@@ -0,0 +1,12 @@
+package headFirstPattern;
+
+public class Duck {
+
+    public void swim() {
+        System.out.println("Я утка, я умею плавать.");
+    }
+
+    public void display() {
+        System.out.println("Если что-то выглядит как утка, то скорее всего это утка.");
+    }
+}
