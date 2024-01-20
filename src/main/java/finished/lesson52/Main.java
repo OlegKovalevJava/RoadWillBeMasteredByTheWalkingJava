29fcb03ef3bda0f0d2aa5d701b79a962512ae7e3
Oleg <kovalyovop@yandex.ru>
2024-01-20 22:12:29 +0300

    lesson53

--- src/main/java/finished/lesson52/Main.java
+++ src/main/java/finished/lesson52/Main.java
@@ -1,9 +1,10 @@
 package finished.lesson52;
 
+import java.util.function.Function;
+
 public class Main {
     public static void main(String[] args) {
-        Runnable r = (args.length > 0 ?
-                () -> System.out.println("Hello " + args[0] + "!") :
-                () -> System.out.println("Hello World"));
+
+
     }
 }
