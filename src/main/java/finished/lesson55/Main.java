f13ce73aa10944bbce2b4ca41b1bd833deabcfec
Oleg <kovalyovop@yandex.ru>
2024-02-29 00:52:16 +0300

    lesson55+1

--- src/main/java/finished/lesson55/Main.java
+++ src/main/java/finished/lesson55/Main.java
@@ -1,25 +1,14 @@
 package finished.lesson55;
 
-import java.util.Arrays;
-import java.util.stream.Stream;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
+        Predicate<String> isFoo = "Java"::equals;
 
-        Integer[] arr0 = Stream.of(1, 2, 10)
-                .toArray(Integer[]::new);
-
-        Integer[] arr1 = Stream.of(1, 2, 10)
-                .toArray(length -> new Integer[length]);
-
-        String[] arr2 = Stream.of("11", "22", "101")
-                .toArray(String[]::new);
-
-        System.out.println(Arrays.toString(arr0));
-
-        System.out.println(Arrays.toString(arr1));
-
-        System.out.println(Arrays.toString(arr2));
+        System.out.println(isFoo.test("Java"));
+        System.out.println(isFoo.test("lava"));
+        System.out.println(isFoo.test("ava"));
 
     }
 }
