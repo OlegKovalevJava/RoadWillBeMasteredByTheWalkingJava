ede1132268d041806ed7b0d5bf0d6cb1340e9821
Oleg <kovalyovop@yandex.ru>
2024-03-01 20:46:53 +0300

    lesson55+1

--- src/main/java/finished/lesson55/Main.java
+++ src/main/java/finished/lesson55/Main.java
@@ -6,9 +6,5 @@ public class Main {
     public static void main(String[] args) {
         Predicate<String> isFoo = "Java"::equals;
 
-        System.out.println(isFoo.test("Java"));
-        System.out.println(isFoo.test("lava"));
-        System.out.println(isFoo.test("ava"));
-
     }
 }
