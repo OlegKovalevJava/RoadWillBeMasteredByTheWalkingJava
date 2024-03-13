9da99ceb364be99ce0450317bd309fadfc3b146d
Oleg <kovalyovop@yandex.ru>
2024-03-13 20:59:01 +0300

    lesson4 update

--- src/main/java/finished/lesson56/Main.java
+++ src/main/java/finished/lesson56/Main.java
@@ -1,10 +1,5 @@
 package finished.lesson56;
 
 public class Main {
-    AutoCloseable autoCloseable = new AutoCloseable() {
-        @Override
-        public void close() throws Exception {
 
-        }
-    };
 }
