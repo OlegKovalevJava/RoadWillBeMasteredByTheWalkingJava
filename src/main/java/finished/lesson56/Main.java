b22c1acc90f730b37239ba50cb5764ca7f6048dc
Oleg <kovalyovop@yandex.ru>
2024-03-18 17:20:06 +0300

    lesson57

--- src/main/java/finished/lesson56/Main.java
+++ src/main/java/finished/lesson56/Main.java
@@ -1,8 +1,16 @@
 package finished.lesson56;
 
+import java.util.stream.*;
+
 public class Main {
     public static void main(String[] args) {
 
-    }
+        AutoCloseable autoCloseable;
+        BaseStream baseStream;
+        Stream stream;
+        IntStream intStream;
+        LongStream longStream;
+        DoubleStream doubleStream;
 
+    }
 }
