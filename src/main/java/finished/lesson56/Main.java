4afb3c814035c4161199a88a7ffe4e9e43f02411
Oleg <kovalyovop@yandex.ru>
2024-03-20 21:43:21 +0300

    lesson57

--- src/main/java/finished/lesson56/Main.java
+++ src/main/java/finished/lesson56/Main.java
@@ -1,22 +1,7 @@
 package finished.lesson56;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        List<String> listPeople = new ArrayList<>();
-        listPeople.add("Tom");
-        listPeople.add("Com");
-        listPeople.add("Som");
-        listPeople.add("Mom");
-
-        listPeople.remove("Som");
-        listPeople.remove(0);
-
-        for (String peoples: listPeople) {
-            System.out.println(peoples);
-        }
 
     }
 }
